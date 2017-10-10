c2dc3d4fcbd9c192a6bcf4d1bb392cd4e35da480
Angelo Villadolid <villadolidangeloryan@gmail.com>
2017-10-10 16:53:58 -0700

    Add files via upload

--- ClientHandler.java
+++ ClientHandler.java
@@ -4,53 +4,136 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-public class ClientHandler implements Runnable {
-
+public class ClientHandler implements Runnable
+{
+	private MyServer parentServer;
 	private Socket socket;
-    private int id;
+    private PrintWriter out;
+    private BufferedReader in;
+    private String nick = null;
 
-    public ClientHandler(Socket socket, int id) {
+    public ClientHandler(Socket socket, MyServer server, String initialNick)
+    {
         this.socket = socket;
-        this.id = id;
-        
+        parentServer = server;
+        nick = initialNick;
+    }
+    
+    public void sendMessage(String message)
+    {
+    	out.println(message);
+    }
+    
+    public void sendList(Integer[] list)
+    {
+    	String message = "#";
+    	for (Integer n : list)
+    		message += ("," + n.intValue());
+    	out.println(message);
+    }
+    
+    public void sendClientList(String[] clientNames)
+    {
+    	String message = "~";
+    	for (String name : clientNames)
+    		message += "," + name;
+    	while (out == null && !socket.isClosed());
+    	out.println(message);
     }
 
 	@Override
-	public void run() {
-		System.out.println("Building connection with client# " + id + " at " + socket);
-		try {
+	public void run()
+	{
+		parentServer.addLog("Building connection with " + nick + " at " + socket);
+		try
+		{
 			InputStreamReader isr = new InputStreamReader(socket.getInputStream());
-            BufferedReader in = new BufferedReader(isr);
+            in = new BufferedReader(isr);
            
             // (outputstream, autoflush)
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            out = new PrintWriter(socket.getOutputStream(), true);
 
             // Send a welcome message to the client.
-            out.println("Welcome Client # " + id);
-            out.println("Enter @ to quit");
-            
+            out.println("Welcome " + nick + ". You can use /nick to change your nickname.");
 
             String msg;
             // waiting for client to send message
-            while (true) {
+            while (true)
+            {
                 msg = in.readLine();
-                if (msg == null || msg.equals("@")) {
+                if (msg == null)
                     break;
+                else if (msg.startsWith("/nick"))
+                {
+                	String oldNick = nick;
+                	if (msg.equals("/nick"))
+                	{
+                		nick = parentServer.getAvailableNick();
+                		parentServer.changeNick(oldNick, nick);
+                		parentServer.updateClients();
+                	}
+                	else if (msg.length() < 21)
+                	{
+                		nick = msg.substring(6);
+                		if (nick.equals(""))
+                			nick = parentServer.getAvailableNick();
+                		if (parentServer.changeNick(oldNick, nick) == 0)
+                		{
+                			nick = oldNick;
+                			out.println("Nickname is already in use");
+                		}
+                		else // Not currently checking for if the old name could not be found.
+                			parentServer.updateClients();
+                	}
+                	else
+                		out.println("Nickname must be less than 15 characters");
                 }
-                System.out.println("Message from client #" + id + ", [" + msg + "]");
+                else if (msg.startsWith("/msg "))
+                {
+                	// Get the username to send the message to
+                	String message = msg.substring(5);
+                	String targetUser;
+                	if (message.startsWith("\""))
+                	{
+                		// Find the second quote
+                		int split = message.substring(1).indexOf("\" ");
+                		targetUser = message.substring(1, split);
+                		message = message.substring(split + 1);
+                	}
+                	else
+                	{
+                		int split = message.indexOf(" ");
+                		targetUser = message.substring(0, split);
+                		message = message.substring(split + 1);
+                	}
+                	parentServer.privateMessage(this, targetUser, message);
+                }
+                else
+                	parentServer.messageReceived(msg, this);
             }
-        } catch (IOException e) {
-            System.out.println("Error client# " + id + ": " + e);
-        } finally {
-            try {
-                socket.close();
-            } catch (IOException e) {
-                System.out.println("Client # :" + id + " ... Couldn't close a socket");
-            }
-            System.out.println("client# " + id + " left");
         }
-		
-		
+		catch (IOException e)
+		{
+            System.out.println("Error " + nick + ": " + e);
+        }
+		finally
+		{
+            try
+            {
+                socket.close();
+                parentServer.removeClient(this);
+            }
+            catch (IOException e)
+            {
+                System.out.println(nick + " ... Couldn't close a socket");
+            }
+            // System.out.println("client #" + id + " left");
+        }
 	}
-
+	
+	@Override
+	public String toString()
+	{
+		return nick;
+	}
 }
--- ServerGUI.java
+++ ServerGUI.java
@@ -3,84 +3,133 @@ import javax.swing.*;
 
 public class ServerGUI extends JFrame
 {
-	private Container con;
-	
-	private JPanel top, mid, bot;
-	
-	private JLabel ipLabel, portLabel;
+	//Top
+	private JLabel infoLabel, ipLabel, portLabel;
 	private JTextField ipField, portField;
-	
+	//Mid
 	private JLabel serverLogLabel, sortedListLabel, clientListLabel;
+	//Bot
 	private JTextArea serverLogArea;
-	private JList sortedList, clientList;
+	private DefaultListModel<Integer> sortedListMod;
+	private JList<Integer> sortedListDisplay;
+	private JList<String> clientList;
 	private JScrollPane logPane, listPane, clientPane;
+	//Other
+	private JLabel broadcastLabel;
+	private JTextField broadcastField;
 	
-	public static void main(String[] arg)
+	public ServerGUI(MyServer parent)
 	{
-		ServerGUI serv = new ServerGUI();
-		serv.setVisible(true);
-	}
-	
-	ServerGUI()
-	{
-		setSize(1000, 700);
+		this.setLayout(null);
+		this.setBounds(10,10,1000,700);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		con = getContentPane();
-		con.setLayout(new GridLayout(3, 1, 5, 5));
 		
 		// ========== TOP PANEL ==========
-		top = new JPanel();
-		top.setLayout(new GridLayout(1, 4, 5, 5));
-		con.add(top);
 		
-		ipLabel = new JLabel("IP : ");
-		top.add(ipLabel);
+		infoLabel = new JLabel("Connection Info:");
+		attach(infoLabel,10,10,600,15);
+		
+		ipLabel = new JLabel("IP:");
+		attach(ipLabel,10,30,25,15);
+		
+		portLabel = new JLabel("Port:");
+		attach(portLabel,300,30,50,15);
 		
 		ipField = new JTextField();
+		attach(ipField,30,30,250,20);
 		ipField.setEditable(false);
-		top.add(ipField);
 		
-		portLabel = new JLabel("Port : ");
-		top.add(portLabel);
-		
-		portField = new JTextField();
+		portField = new JTextField("8000");
+		attach(portField,333,30,100,20);
 		portField.setEditable(false);
-		top.add(portField);
 		
 		// ========== MID PANEL ==========
-		mid = new JPanel();
-		mid.setLayout(new GridLayout(1, 3, 5, 5));
-		con.add(mid);
 		
 		serverLogLabel = new JLabel("Server log : ");
-		mid.add(serverLogLabel);
+		attach(serverLogLabel,10,50,450,20);
 		
 		sortedListLabel = new JLabel("Sorted list : ");
-		mid.add(sortedListLabel);
+		attach(sortedListLabel,525,50,450,20);
 		
 		clientListLabel = new JLabel("Client List : ");
-		mid.add(clientListLabel);
+		attach(clientListLabel,800,50,450,20);
 		
 		// ========== BOT PANEL ===========
-		bot = new JPanel();
-		bot.setLayout(new GridLayout(1, 3, 5, 5));
-		con.add(bot);
-		
+
 		serverLogArea = new JTextArea();
 		logPane = new JScrollPane(serverLogArea);
-		bot.add(logPane);
+		attach(logPane,10,75,500,500);
 		
-		sortedList = new JList();
-		listPane = new JScrollPane(sortedList);
-		bot.add(listPane);
+		sortedListMod = new DefaultListModel<>();
+		sortedListDisplay = new JList<>(sortedListMod);
+		listPane = new JScrollPane(sortedListDisplay);
+		attach(listPane,525,75,100,500);
 		
-		clientList = new JList();
+		clientList = new JList<>();
 		clientPane = new JScrollPane(clientList);
-		bot.add(clientPane);
+		attach(clientPane,800,75,150,500);
+		
+		// ========== OTHER PANEL ===========
+		
+		broadcastLabel = new JLabel("Enter Message To Broadcast : ");
+		attach(broadcastLabel,10,580,500,20);
+		
+		broadcastField = new JTextField();
+		broadcastField.addActionListener(parent);
+		attach(broadcastField,10,600,500,20);
+	}
+	
+	public void attach(Component c, int x, int y, int w, int h)
+	{
+		add(c);
+		c.setBounds(x, y, w,h);
 	}
 	
 	public void setIP(String ip)
 	{
-		
+		ipField.setText(ip);
+	}
+	
+	public void setPort(String port)
+	{
+		portField.setText(port);
+	}
+	
+	public void addLog(String log)
+	{
+		serverLogArea.append(log + "\n");
+	}
+	
+	public void clearMessageEntry()
+	{
+		broadcastField.setText("");
+	}
+	
+	public Integer[] addListItem(int num)
+	{
+		for (int i = 0; i < sortedListMod.size(); i++)
+		{
+			if (sortedListMod.getElementAt(i).intValue() > num)
+			{
+				sortedListMod.add(i, num);
+				Integer[] temp = new Integer[sortedListMod.size()];
+				for (int j = 0; j < sortedListMod.size(); j++)
+					temp[j] = sortedListMod.get(j);
+				return temp;
+			}
+		}
+		// If this point is reached, the number should be added to the end of
+		// the ArrayList because it's either empty or the number is bigger than
+		// the last value.
+		sortedListMod.addElement(num);
+		Integer[] temp = new Integer[sortedListMod.size()];
+		for (int j = 0; j < sortedListMod.size(); j++)
+			temp[j] = sortedListMod.get(j);
+		return temp;
+	}
+	
+	public void setClients(String[] clients)
+	{
+		clientList.setListData(clients);
 	}
 }
